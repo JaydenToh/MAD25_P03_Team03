349ab6703333c723446d6bffcd51719a51641d1e
Zhan Yang <148023902+LiewZhanYang@users.noreply.github.com>
2025-11-19 21:52:36 +0800

    Add full Android project files

---
+++ app/src/main/java/np/ict/mad/mad25_p03_team03/MainActivity.kt
@@ -0,0 +1,47 @@
+package np.ict.mad.mad25_p03_team03
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import np.ict.mad.mad25_p03_team03.ui.theme.MAD25_P03_Team03Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MAD25_P03_Team03Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MAD25_P03_Team03Theme {
+        Greeting("Android")
+    }
+}
