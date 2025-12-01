bb6eeef2fee5706c735ef643e85086e32b4bf8dc
JaydenToh <s10241868@connect.np.edu.sg>
2025-12-01 16:01:59 +0800

    Structuring README file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,6 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.3")
 
     implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
-    implementation("com.google.firebase:firebase-analytics")
+    //implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-firestore")
 }
