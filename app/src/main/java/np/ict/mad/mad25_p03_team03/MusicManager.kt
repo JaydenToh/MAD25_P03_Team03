5e51fa3a687daab985669402296b9d857d16e0e1
JaydenToh <s10241868@connect.np.edu.sg>
2026-02-08 08:14:11 +0800

    Updating the overall the music manager so users can skip, pause, or play music still even when they leave the screen. Adding a bottomplayerbar file to allow it to seperate from the songlibrary screen.

--- app/src/main/java/np/ict/mad/mad25_p03_team03/MusicManager.kt
+++ app/src/main/java/np/ict/mad/mad25_p03_team03/MusicManager.kt
@@ -2,13 +2,38 @@ package np.ict.mad.mad25_p03_team03
 
 import android.content.Context
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
 
 object MusicManager {
     private var player: ExoPlayer? = null
-    // Get the existing player or create a new one if missing
+
+    var currentSong by mutableStateOf<SongItem?>(null)
+    var isPlaying by mutableStateOf(false)
+    var playlist by mutableStateOf(listOf<SongItem>())
+    var currentIndex by mutableStateOf(-1)
+
     fun getPlayer(context: Context): ExoPlayer {
         if (player == null) {
             player = ExoPlayer.Builder(context).build()
+
+            player?.addListener(object : Player.Listener {
+                override fun onIsPlayingChanged(playing: Boolean) {
+                    isPlaying = playing
+                }
+
+                override fun onMediaItemTransition(mediaItem: MediaItem?, reason: Int) {
+                    // Updates song when music changes automatically
+                    val index = player?.currentMediaItemIndex ?: -1
+                    if (index != -1 && index < playlist.size) {
+                        currentIndex = index
+                        currentSong = playlist[index]
+                    }
+                }
+            })
         }
         return player!!
     }
--- app/src/main/java/np/ict/mad/mad25_p03_team03/SongLibrary.kt
+++ app/src/main/java/np/ict/mad/mad25_p03_team03/SongLibrary.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -28,11 +27,9 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
@@ -81,7 +78,6 @@ fun SongLibraryScreen(
     val exoPlayer = remember { MusicManager.getPlayer(context) }
 
     // Track playing state
-    var currentPlayingUrl by remember { mutableStateOf<String?>(null) }
     var isPlaying by remember { mutableStateOf(false) }
     var currentSong by remember { mutableStateOf(-1) }
     var repeat by remember { mutableStateOf(ExoPlayer.REPEAT_MODE_OFF) }
